5a5d07536ad4adb26dd51496a58457de44e9cb1c
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2023-11-03 22:37:51 +0100

    add comments

--- javacard/src/notreprojet/NotreProjet.java
+++ javacard/src/notreprojet/NotreProjet.java
@@ -16,39 +16,84 @@
 package notreprojet;
 
 import javacard.framework.*;
-import javacard.security.*;
+import javacard.security.KeyPair;
+import javacard.security.RSAPrivateKey;
+import javacard.security.RSAPublicKey;
+import javacard.security.Signature;
 
 /**
- *
+ * The class implement a smart card that can sign messages
+ * It is protected by a PIN code and owns a RSA key pair.
  */
-
 public class NotreProjet extends Applet {
-    public static final byte[] DEFAULT_PIN = {0x01, 0x02, 0x03, 0x04};
-    public static final short KEY_BITS = 512;
-    /* constants declaration */
-    // code of CLA byte in the command APDU header
-    final static byte CLA = (byte) 0xB0;
-    // codes of INS byte in the command APDU header
-
-    final static byte INS_LOGIN = (byte) 0x01;
-    final static byte INS_MODIFY_PIN = (byte) 0x02;
-    final static byte INS_SIGN_MESSAGE = (byte) 0x04;
-    final static byte INS_SEND_PUBLIC_KEY = (byte) 0x05;
-
-    final static byte INS_FACTORY_RESET = (byte) 0x06;
-    final static byte PIN_LENGTH = 4;
-    final static byte MAX_PIN_RETRY = 3;
-    OwnerPIN pin;
-    private KeyPair keyPair;
-
-    private RSAPrivateKey privateKey;
-
-    private RSAPublicKey publicKey;
-
 
     /**
-     * Only this class's install method should create the applet object.
+     * Default PIN code
      */
+    public static final byte[] DEFAULT_PIN = {0x01, 0x02, 0x03, 0x04};
+
+    /**
+     * Size of the RSA key pair
+     */
+    public static final short KEY_BITS = 512;
+
+    /**
+     * CLA byte of the applet
+     */
+    final static byte CLA = (byte) 0xB0;
+
+    // codes of INS byte in the command APDU header
+
+    /**
+     * INS byte of the login command
+     */
+    final static byte INS_LOGIN = (byte) 0x01;
+
+    /**
+     * INS byte of the modify pin command
+     */
+    final static byte INS_MODIFY_PIN = (byte) 0x02;
+
+    /**
+     * INS byte of the sign message command
+     */
+    final static byte INS_SIGN_MESSAGE = (byte) 0x04;
+
+    /**
+     * INS byte of the send public key command
+     */
+    final static byte INS_SEND_PUBLIC_KEY = (byte) 0x05;
+
+    /**
+     * INS byte of the factory reset command
+     */
+    final static byte INS_FACTORY_RESET = (byte) 0x06;
+
+    /**
+     * Length of the PIN code
+     */
+    final static byte PIN_LENGTH = 4;
+
+    /**
+     * Maximum number of tries for the PIN code
+     */
+    final static byte MAX_PIN_RETRY = 3;
+
+    /**
+     * PIN object used to protect the applet
+     */
+    OwnerPIN pin;
+
+    /**
+     * Private key of the key pair
+     */
+    private RSAPrivateKey privateKey;
+
+    /**
+     * Public key of the key pair
+     */
+    private RSAPublicKey publicKey;
+
 
     protected NotreProjet() {
         pin = new OwnerPIN(MAX_PIN_RETRY, PIN_LENGTH);
@@ -56,27 +101,9 @@ public class NotreProjet extends Applet {
         register();
     }
 
-    /**
-     * Installs this applet.
-     *
-     * @param bArray  the array containing installation parameters
-     * @param bOffset the starting offset in bArray
-     * @param bLength the length in bytes of the parameter data in bArray
-     */
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         // Create the Signer applet instance
         new NotreProjet();
-    }
-
-    private void setDefaultPin() {
-        pin.update(DEFAULT_PIN, (short) 0, PIN_LENGTH);
-    }
-
-    private void generateRSAKeys() {
-        keyPair = new KeyPair(KeyPair.ALG_RSA, KEY_BITS);
-        keyPair.genKeyPair();
-        privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        publicKey = (RSAPublicKey) keyPair.getPublic();
     }
 
     public boolean select() {
@@ -90,11 +117,12 @@ public class NotreProjet extends Applet {
 
     public void process(APDU apdu) {
         if (selectingApplet()) {
-            // ignore if the applet is being selected
+            // If the applet is selected, we do nothing
             ISOException.throwIt(ISO7816.SW_NO_ERROR);
         }
 
         byte[] buffer = apdu.getBuffer();
+        // Ensure all the incoming data has been received
         apdu.setIncomingAndReceive();
 
         if (buffer[ISO7816.OFFSET_CLA] != CLA) {
@@ -102,6 +130,7 @@ public class NotreProjet extends Applet {
             ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
         }
 
+        // Process the command based on the INS byte
         switch (buffer[ISO7816.OFFSET_INS]) {
             case INS_LOGIN:
                 login(apdu);
@@ -121,11 +150,20 @@ public class NotreProjet extends Applet {
         }
     }
 
+    /**
+     * Factory reset the applet
+     * Reset the PIN code to the default one and generate a new RSA key pair
+     */
     private void factoryReset() {
         setDefaultPin();
         generateRSAKeys();
     }
 
+    /**
+     * Authenticate the user with the PIN code to the applet
+     *
+     * @param apdu data received from the terminal
+     */
     private void login(APDU apdu) {
         if (pin.isValidated()) {
             ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
@@ -135,6 +173,27 @@ public class NotreProjet extends Applet {
         checkPin(buffer);
     }
 
+    /**
+     * Check if the user is authenticated
+     */
+    private void checkLogin() {
+        if (!pin.isValidated()) {
+            ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+        }
+    }
+
+    /**
+     * Initialize the PIN code and the RSA key pair
+     */
+    private void setDefaultPin() {
+        pin.update(DEFAULT_PIN, (short) 0, PIN_LENGTH);
+    }
+
+    /**
+     * Check if the PIN code is valid
+     *
+     * @param buffer data received from the terminal
+     */
     private void checkPin(byte[] buffer) {
         if (buffer[ISO7816.OFFSET_LC] != PIN_LENGTH) {
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
@@ -145,6 +204,40 @@ public class NotreProjet extends Applet {
         }
     }
 
+    /**
+     * Modify the PIN code
+     *
+     * @param apdu data received from the terminal
+     */
+    private void modifyPin(APDU apdu) {
+        checkLogin();
+        byte[] buffer = apdu.getBuffer();
+
+        if (buffer[ISO7816.OFFSET_LC] != PIN_LENGTH) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        // Comment maybe useless need more research
+        apdu.setIncomingAndReceive();
+
+        pin.update(buffer, ISO7816.OFFSET_CDATA, PIN_LENGTH);
+    }
+
+    /**
+     * Generate a RSA key pair used to sign messages
+     */
+    private void generateRSAKeys() {
+        KeyPair keyPair = new KeyPair(KeyPair.ALG_RSA, KEY_BITS);
+        keyPair.genKeyPair();
+        privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        publicKey = (RSAPublicKey) keyPair.getPublic();
+    }
+
+    /**
+     * Sign the fingerprint of the message with the RSA private key
+     *
+     * @param apdu data received from the terminal
+     */
     private void signMessage(APDU apdu) {
         checkLogin();
         byte[] buffer = apdu.getBuffer();
@@ -164,11 +257,24 @@ public class NotreProjet extends Applet {
 
     }
 
+    /**
+     * Send the public key to the terminal
+     *
+     * @param apdu data received from the terminal
+     */
     private void sendPublicKey(APDU apdu) {
         short keyToSend = serializeKey(publicKey, apdu.getBuffer(), ISO7816.OFFSET_CDATA);
         apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, keyToSend);
     }
 
+    /**
+     * Serialize a RSA public key
+     *
+     * @param key    the RSA public key to serialize
+     * @param buffer the buffer to write the serialized key
+     * @param offset the offset to write the serialized key
+     * @return the length of the serialized key
+     */
     private short serializeKey(RSAPublicKey key, byte[] buffer, short offset) {
         // Code from the thread in stackoverflow :
         // https://stackoverflow.com/questions/42690733/javacard-send-rsa-public-key-in-apdu
@@ -178,25 +284,5 @@ public class NotreProjet extends Applet {
         short modLen = key.getModulus(buffer, (short) (offset + 4 + expLen));
         Util.setShort(buffer, (short) (offset + 2 + expLen), modLen);
         return (short) (4 + expLen + modLen);
-    }
-
-    private void checkLogin() {
-        if (!pin.isValidated()) {
-            ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
-        }
-    }
-
-    private void modifyPin(APDU apdu) {
-        checkLogin();
-        byte[] buffer = apdu.getBuffer();
-
-        if (buffer[ISO7816.OFFSET_LC] != PIN_LENGTH) {
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
-        }
-
-        // Comment maybe useless need more research
-        apdu.setIncomingAndReceive();
-
-        pin.update(buffer, ISO7816.OFFSET_CDATA, PIN_LENGTH);
     }
 }
